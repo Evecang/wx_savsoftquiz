240b5868cd1e65712f298d4e910e8d95b587d0c2
LinYiyu24K <793988238@qq.com>
2019-04-16 18:40:12 +0800

    在index.js添加bgImgAbosoluteUrl字段用于存储背景图片绝对路径，将page样式设置为height:100%，使得页面必占满屏幕

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,7 +10,8 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     loading:false,
     showLogin:false,
-    userData:null,
+    userData: null, 
+    bgImgAbosoluteUrl:  '',
     editUser:{
       password:'',
       first_name:'',
@@ -145,16 +146,20 @@ Page({
     var that = this
     
     if (app.globalData.userInfo) {
+
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
+        bgImgAbosoluteUrl: app.globalData.userInfo.avatarUrl
       })
     } else if (this.data.canIUse){
+
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true,
+          bgImgAbosoluteUrl: app.globalData.userInfo.avatarUrl
         })
       }
     } else {
