e1a88e6a1e093a83ec2901add8f329ccaa29fdd1
Evecang <547624569@qq.com>
2019-04-17 15:15:22 +0800

    准备做quiz_detail

--- pages/quiz/quiz_list.js
+++ pages/quiz/quiz_list.js
@@ -9,7 +9,95 @@ Page({
    */
   data: {
     search:'long answer test',
-    result:[],
+    result:[{
+      'quid': '20',
+      'quiz_name': 'long answer test 3',
+      'description': '<p>测试longanswer成绩、显示、作答功能。</p>',
+      'start_date': '1554399143',
+      'end_date': '1585935143',
+      'gids': '1',
+      'qids': '22,24,28',
+      'noq': '3',
+      'correct_score': '15,10,25',
+      'incorrect_score': '0,0,0',
+      'ip_address': '',
+      'duration': '3000',
+      'maximum_attempts': '100',
+      'pass_percentage': '60',
+      'view_answer': '1',
+      'camera_req': '0',
+      'question_selection': '0',
+      'gen_certificate': '0',
+      'certificate_text': {},
+      'with_login': '1',
+      'quiz_template': 'Default', 
+      },{
+        'quid': '19',
+        'quiz_name': 'cloze1',
+        'description': '',
+        'start_date': '1554102399',
+        'end_date': '1585638399',
+        'gids': '1',
+        'qids': '26,27',
+        'noq': '2',
+        'correct_score': '10,15',
+        'incorrect_score': '0,0',
+        'ip_address': '',
+        'duration': '99999',
+        'maximum_attempts': '100',
+        'pass_percentage': '50',
+        'view_answer': '1',
+        'camera_req': '0',
+        'question_selection': '0',
+        'gen_certificate': '0',
+        'certificate_text': {},
+        'with_login': '1',
+        'quiz_template': 'Default',
+      },{
+        'quid': '18',
+        'quiz_name': 'chengji',
+        'description': '',
+        'start_date': '1554083931',
+        'end_date': '1585619931',
+        'gids': '1',
+        'qids': '7,17,18,19',
+        'noq': '4',
+        'correct_score': '1,2,3,4',
+        'incorrect_score': '0,0,0,0',
+        'ip_address': '',
+        'duration': '10',
+        'maximum_attempts': '10',
+        'pass_percentage': '50',
+        'view_answer': '1',
+        'camera_req': '0',
+        'question_selection': '0',
+        'gen_certificate': '0',
+        'certificate_text': {},
+        'with_login': '1',
+        'quiz_template': 'Default',
+      },{
+        'quid' : '17',
+        'quiz_name' : 'long answer test 2',
+        'description' : '',
+        'start_date' : '1554082520',
+        'end_date' : '1585618520',
+        'gids' : '1',
+        'qids' : '20,16,18,19,24',
+        'noq' : '5',
+        'correct_score' : '5,5,1,1,5',
+        'incorrect_score' : '0,0,0,0,0',
+        'ip_address' : '',
+        'duration' : '10',
+        'maximum_attempts' : '10',
+        'pass_percentage' : '50',
+        'view_answer' : '1',
+        'camera_req' : '0',
+        'question_selection' : '0',
+        'gen_certificate' : '0',
+        'certificate_text' : {},
+        'with_login' : '1',
+        'quiz_template' : 'Default',
+      }],
     bgColor: ['#dff0d8', '#fcf8e3', '#d9edf7','#f2dcf2'],
     fontColor: ['#3c763d', '#8a6d3b', '#31708f','#a94442'],
     userData: app.globalData.userData
@@ -37,12 +125,46 @@ Page({
           result: res.data.result,
           userData: res.data.user
         })
-
       },
       fail(res) {
         console.log('获取考试列表失败')
       }
     })
+  },
+  quizDetail(e){
+    var dataset = e.currentTarget.dataset
+    var quid = dataset.quid
+    let cookie = wx.getStorageSync('cookieKey')
+    let header = {}
+    if (cookie) {
+      header.Cookie = cookie;
+    }
+    wx.request({
+      url: URL + 'quiz/wx_quiz_detail/' + quid,
+      header:header,
+      method:'GET',
+      success:res =>{
+        console.log(res)
+        wx.setStorage({
+          key: 'quizDetail',
+          data: res.data
+        })
+        //跳转至详情页面
+        wx.navigateTo({
+          url: '../quiz_detail/quiz_detail'
+        })
+      },
+      fail:res =>{
+        console.log('get quid detail failed')
+      }
+    })
+
+  },
+  editQuiz(e){
+    console.log('edit')
+  },
+  removeQuiz(e){
+    console.log('remove')
   },
 
   /**
