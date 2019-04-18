b68dcfcf37fb23765746b5c42b6e52a74087ddb3
LinYiyu24K <793988238@qq.com>
2019-04-18 10:07:38 +0800

    修改背景图为 cover

--- pages/result/result_list.js
+++ pages/result/result_list.js
@@ -1,18 +1,48 @@
 // pages/result/result_list.js
+const app = getApp()
+const URL = app.globalData.url;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    result:null,
+    quiz_list:null,
+    userData: app.globalData.userData,
+    bgColor: ['#dff0d8', '#fcf8e3', '#d9edf7', '#f2dcf2'],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let cookie = wx.getStorageSync('cookieKey')
+    let header = { 'Content-type': 'application/json'}
+    if (cookie) {
+      header.Cookie = cookie;
+    }
+    let that = this
+    wx.request({
+      url: URL + 'result/wx_index/',
+      method: 'POST',
+      header: header,
+      success(res) {
+        console.log(res)
+        if (res && res.header && res.header['Set-Cookie']) {
+          wx.setStorageSync('cookieKey', res.header['Set-Cookie']);   //保存Cookie到Storage
+        }
+        that.setData({
+          result: res.data.result,
+          quiz_list: res.data.quiz_list
+        })
 
+      },
+      fail(res) {
+        console.log('初始化考试结果列表失败')
+      }
+    })
   },
 
   /**
